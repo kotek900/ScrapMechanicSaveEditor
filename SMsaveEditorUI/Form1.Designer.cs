b2e8979f017e527cbc12be08710bb43181df7118
kotek900 <adam1gmiterek@gmail.com>
2020-05-23 17:54:45 +0200

    Dodaj pliki projektów.

---
+++ SMsaveEditorUI/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace SMsaveEditorUI {
+    partial class Form1 {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gameTickToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveGameVersionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.flagsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.saveDataToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(828, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveDataToolStripMenuItem
+            // 
+            this.saveDataToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.modsToolStripMenuItem,
+            this.seedToolStripMenuItem,
+            this.gameTickToolStripMenuItem,
+            this.saveGameVersionToolStripMenuItem,
+            this.flagsToolStripMenuItem});
+            this.saveDataToolStripMenuItem.Name = "saveDataToolStripMenuItem";
+            this.saveDataToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.saveDataToolStripMenuItem.Text = "SaveData";
+            // 
+            // modsToolStripMenuItem
+            // 
+            this.modsToolStripMenuItem.Name = "modsToolStripMenuItem";
+            this.modsToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.modsToolStripMenuItem.Text = "Mods";
+            // 
+            // seedToolStripMenuItem
+            // 
+            this.seedToolStripMenuItem.Name = "seedToolStripMenuItem";
+            this.seedToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.seedToolStripMenuItem.Text = "Seed";
+            this.seedToolStripMenuItem.Click += new System.EventHandler(this.seedToolStripMenuItem_Click_1);
+            // 
+            // gameTickToolStripMenuItem
+            // 
+            this.gameTickToolStripMenuItem.Name = "gameTickToolStripMenuItem";
+            this.gameTickToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.gameTickToolStripMenuItem.Text = "GameTick";
+            // 
+            // saveGameVersionToolStripMenuItem
+            // 
+            this.saveGameVersionToolStripMenuItem.Name = "saveGameVersionToolStripMenuItem";
+            this.saveGameVersionToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.saveGameVersionToolStripMenuItem.Text = "SaveGameVersion";
+            // 
+            // flagsToolStripMenuItem
+            // 
+            this.flagsToolStripMenuItem.Name = "flagsToolStripMenuItem";
+            this.flagsToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.flagsToolStripMenuItem.Text = "Flags";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 140);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(466, 259);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 275);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "3D view goes here";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 411);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "SM Save Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seedToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gameTickToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveGameVersionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem flagsToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SMsaveEditorUI/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace SMsaveEditorUI {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void gameDataToolStripMenuItem_Click(object sender, EventArgs e) {
+
+        }
+
+        string SQLfilePath=":memory:";
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            dlg.InitialDirectory = "c:\\";
+            dlg.Filter = "All files (*.*)|*.*|db files (*.db)|*.db";
+            dlg.FilterIndex = 2;
+            dlg.RestoreDirectory = true;
+
+            if (dlg.ShowDialog() == DialogResult.OK) {
+                string fileName;
+                fileName = dlg.FileName;
+                SQLfilePath = fileName;
+            }
+        }
+
+        private void seedToolStripMenuItem_Click(object sender, EventArgs e) {
+            
+        }
+
+        private void seedToolStripMenuItem_Click_1(object sender, EventArgs e) {
+            var con = new SQLiteConnection("Data Source=" + SQLfilePath);
+            con.Open();
+            var cmd = new SQLiteCommand("SELECT * FROM Game;", con);
+            SQLiteDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+                MessageBox.Show(rdr.GetInt32(2).ToString());
+            con.Dispose();
+            rdr.Dispose();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
